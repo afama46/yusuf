fec2f90c0bc7bea7edd69e2d20aa3f1830b7c9fd
afama46 <109748590+afama46@users.noreply.github.com>
2022-07-21 17:20:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#list
+student_list = ["sela","nike","james","paul","mary"]
+print(student_list)
+
+student_id = [100, 101, 102, 103, 104, 105]
+print(student_id)
+combined_list = ['John', 7, 10, 'Abdul',True, "true"]
+print(combined_list)
+print(combined_list[4])
+print(combined_list[0:5])
+print(combined_list[6-1])
+print(student_id[-1])
+#print(student_id[-4])
+
+student_id.sort(reverse=True)
+print(student_id)
+student_list = student_list.sort(reverse=False)
+print(student_list)
+#tuple
+list_of_cars = ("toyota","honda","benz")
+print(list_of_cars)
+#dictionary
+student_details = {
+    "1":"sulaimon yusuf",
+    "2":"hassan yemi",
+    "3":"yusuf saad",
+    "4":"ibrahim balogun",
+}
+print(student_details)
+#set
+fruits = {"apple", "orange","pawpaw","pear"}
+print(fruits)
+print(fruits.add("sugarcane"))
+print(fruits)
+print(fruits.update("guava"))
+print("fruit")
+
+
